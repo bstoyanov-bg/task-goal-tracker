686d15624d7aeb59c1e0bc4362ab5dabde7fc385
Bozhidar Stoyanov <stoyanov.bojidar@gmail.com>
2025-02-21 11:13:28 +0200

    Initial monorepo setup with frontend and backend

---
+++ backend/Models/WeatherForecastModel.cs
@@ -0,0 +1,9 @@
+﻿namespace backend.Models
+{
+    public class WeatherForecastModel
+    {
+        public DateTime Date { get; set; }
+        public int TemperatureC { get; set; }
+        public string? Summary { get; set; }
+    }
+}
