296732856b14ae075ba9a7717512c0bf82cef3e6
Bozhidar Stoyanov <stoyanov.bojidar@gmail.com>
2025-02-21 21:47:18 +0200

    Create components for Authentication (Login, Register and services), Components for Goals and Tasks. Setup routes.

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,21 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { GoalListComponent } from './goals/goal-list/goal-list.component';
+import { NgModule } from '@angular/core';
+// import { AuthGuard } from './auth/auth.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'goals', component: GoalListComponent },
+//   { path: 'goals', component: GoalListComponent, canActivate: [AuthGuard] },
+  { path: '', redirectTo: '/goals', pathMatch: 'full' }
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+  })
+
+export class AppRoutingModule { }
---
+++ frontend/src/app/goals/goal-detail/goal-detail.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-goal-detail',
+  imports: [],
+  templateUrl: './goal-detail.component.html',
+  styleUrl: './goal-detail.component.scss'
+})
+export class GoalDetailComponent {
+
+}
